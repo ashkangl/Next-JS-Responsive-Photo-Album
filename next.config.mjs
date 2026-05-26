50f0b4cf8ebb55d39f7f7a96e7961eeff84ee139
ashkangl <ashkan72golzad@gmail.com>
2026-05-27 00:52:22 +0330

    new config

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
     reactStrictMode:false,
     images: {
-        formats: ['image/webp', 'image/avif'],
-        unoptimized: true,
         domains: ['https://fastly.picsum.photos'],
     },
 };
