6e078fc75951761645f229b8d501a72e6dcdcd97
ashkangl <ashkan72golzad@gmail.com>
2026-05-27 18:14:13 +0330

    write clean

--- src/component/Grid.js
+++ src/component/Grid.js
@@ -1,71 +1,32 @@
 
 const Grid = () => {
+
+    const images = [
+        "https://picsum.photos/id/1/800/600",
+        "https://picsum.photos/id/2/800/600",
+        "https://picsum.photos/id/3/800/600",
+        "https://picsum.photos/id/4/800/600",
+        "https://picsum.photos/id/5/800/600",
+        "https://picsum.photos/id/6/800/600",
+        "https://picsum.photos/id/7/800/600",
+        "https://picsum.photos/id/8/800/600",
+        "https://picsum.photos/id/9/800/600",
+        "https://picsum.photos/id/10/800/600",
+        "https://picsum.photos/id/11/800/600",
+        "https://picsum.photos/id/12/800/600",
+        "https://picsum.photos/id/13/800/600",
+        "https://picsum.photos/id/14/800/600",
+        "https://picsum.photos/id/15/800/600",
+        "https://picsum.photos/id/16/800/600",
+    ]
     return(
         <div className="grid md:grid-cols-4 grid-cols-2 w-full md:gap-2 gap-1 pb-16">
-            
-                    <div>
-                        <img src="https://picsum.photos/id/1/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
+            {images.map((img,index) => (
+            <div key={index}>
+                <img src={img} className="h-full w-full object-cover transition duration-500 group-hover:scale-110" alt="picsum" />
+            </div>
 
-                    <div>
-                        <img src="https://picsum.photos/id/2/800/600" className="shadow-lg rounded-xl  hover:scale-105 duration-300" alt="picsum" />
-                    </div>
-
-                    <div>
-                        <img src="https://picsum.photos/id/3/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
-
-                    <div>
-                        <img src="https://picsum.photos/id/4/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
-
-                    <div>
-                        <img src="https://picsum.photos/id/5/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
-
-                    <div>
-                        <img src="https://picsum.photos/id/6/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
-
-                    <div>
-                        <img src="https://picsum.photos/id/7/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
-
-                    <div>
-                        <img src="https://picsum.photos/id/8/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
-
-                    <div>
-                        <img src="https://picsum.photos/id/9/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
-
-                    <div>
-                        <img src="https://picsum.photos/id/10/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
-
-                    <div>
-                        <img src="https://picsum.photos/id/11/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
-
-                    <div>
-                        <img src="https://picsum.photos/id/12/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
-
-                    <div>
-                        <img src="https://picsum.photos/id/13/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
-
-                    <div>
-                        <img src="https://picsum.photos/id/14/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
-
-                    <div>
-                        <img src="https://picsum.photos/id/15/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
-
-                    <div>
-                        <img src="https://picsum.photos/id/16/800/600" className="shadow-lg rounded-xl hover:scale-105 duration-300" alt="picsum" />
-                    </div>
+            ))}
         </div>
     )
 }
